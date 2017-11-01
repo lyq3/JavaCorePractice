367fbe1926b18594a72957db67bfb19689d9f362
罗云强 <99190092@qq.com>
2017-11-01 22:02:52 +0800

    Lambad表达式

---
+++ src/main/java/com/lyq3/lambda/LambdaClass.java
@@ -0,0 +1,13 @@
+package com.lyq3.lambda;
+
+public class LambdaClass {
+    /**
+     * 以函数式接口为参数的方法
+     * @param a
+     * @param b
+     * @param lam
+     */
+    public void parmWhthLambdaInterface(String a, String b,LambdaInterface lam){
+        lam.one(a,b);
+    }
+}
