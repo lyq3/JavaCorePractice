638b336d62ce455509b7616ce6f59745e5d1e9dd
罗云强 <99190092@qq.com>
2017-11-02 22:38:18 +0800

    Lambda

--- src/main/java/com/lyq3/lambda/TestLambda.java
+++ src/main/java/com/lyq3/lambda/TestLambda.java
@@ -1,8 +1,11 @@
 package com.lyq3.lambda;
 
 
+import javax.swing.*;
+import java.io.IOException;
+
 public class TestLambda {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         LambdaClass lc = new LambdaClass();
         lc.parmWhthLambdaInterface("参数1","参数2",(a,b) -> {
             System.out.println(a+b);
@@ -10,5 +13,9 @@ public class TestLambda {
         });
 
         lc.testFunctionQuote("测试方法引用",System.out :: println);//测试推测：使用方法引用，引用的方法参数和返回值因和原接口方法一致
+
+        Timer t = new Timer(1000,System.out :: println);
+        t.start();
+        System.in.read();
     }
 }
